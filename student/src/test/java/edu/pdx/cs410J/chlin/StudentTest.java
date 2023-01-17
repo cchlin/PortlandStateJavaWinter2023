80c0aebbf8c4f483da687eceda1edf7fcf9d708b
Cheng Lin <chlin@pdx.edu>
2023-01-16 22:20:20 -0800

    project implemented and ./mnvw cleaned

--- student/src/test/java/edu/pdx/cs410J/chlin/StudentTest.java
+++ student/src/test/java/edu/pdx/cs410J/chlin/StudentTest.java
@@ -22,4 +22,10 @@ public class StudentTest
     assertThat(pat.getName(), equalTo(name));
   }
 
+  @Test
+  void studentSaysTooMuchWork() {
+    Student student = new Student("Name", new ArrayList<>(), 0.0, "Doesn't matter");
+    assertThat(student.says(), equalTo("This class is too much work"));
+  }
+
 }
