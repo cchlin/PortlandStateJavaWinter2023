8191e072ac4010e9b4bde7a1a2dccd8c695446b0
Cheng Lin <chlin@pdx.edu>
2023-03-20 17:47:51 -0700

    changed the project directory name

---
+++ airline-android-example/app/src/main/java/edu/pdx/cs410J/chlin/CalculatorActivity.java
@@ -0,0 +1,57 @@
+package edu.pdx.cs410J.chlin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    static final String SUM_VALUE = "SUM";
+    private Integer sum;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+
+    public void computeSum(View view) {
+        EditText leftOperandEditText = findViewById(R.id.leftOperand);
+        EditText rightOperandEditText = findViewById(R.id.rightOperand);
+
+        String leftOperandString = leftOperandEditText.getText().toString();
+        String rightOperandString = rightOperandEditText.getText().toString();
+
+        int leftOperand;
+        try {
+            leftOperand = Integer.parseInt(leftOperandString);
+        } catch (NumberFormatException ex) {
+            Toast.makeText(this, "Invalid Number: " + leftOperandString, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        int rightOperand;
+        try {
+            rightOperand = Integer.parseInt(rightOperandString);
+        } catch (NumberFormatException ex) {
+            Toast.makeText(this, "Invalid Number: " + rightOperandString, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        sum = leftOperand + rightOperand;
+
+        TextView resultEditText = findViewById(R.id.result);
+        resultEditText.setText(String.valueOf(sum));
+    }
+
+    public void returnToMain(View view) {
+        Intent data = new Intent();
+        data.putExtra(SUM_VALUE, this.sum);
+        setResult(RESULT_OK, data);
+        finish();
+    }
+}
---
+++ airline-android-example/app/src/main/java/edu/pdx/cs410J/chlin/MainActivity.java
@@ -0,0 +1,99 @@
+package edu.pdx.cs410J.chlin;
+
+import static edu.pdx.cs410J.chlin.CalculatorActivity.SUM_VALUE;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int GET_SUM = 42;
+    private ArrayAdapter<Integer> sums;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listOfSums = findViewById(R.id.sums);
+        List<Integer> sumsFromFile = null;
+        try {
+            sumsFromFile = readSumFromFile();
+        } catch (IOException e) {
+            Toast.makeText(this, "While reading file: " + e, Toast.LENGTH_LONG).show();
+        }
+        sums = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, sumsFromFile);
+        listOfSums.setAdapter(sums);
+    }
+
+    private List<Integer> readSumFromFile() throws IOException {
+        List<Integer> sums = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(getSumsFile()))) {
+            for (String line = br.readLine(); line != null; line = br.readLine()) {
+                sums.add(Integer.parseInt(line));
+            }
+        }
+
+        return sums;
+    }
+
+    public void launchCalculator(View view) {
+        startActivityForResult(new Intent(this, CalculatorActivity.class), GET_SUM);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == GET_SUM) {
+               if (data != null) {
+                   Integer sum = data.getSerializableExtra(SUM_VALUE, Integer.class);
+                   if (sum != null) {
+                       this.sums.add(sum);
+                       try {
+                           writeSumsToIntergalStorage();
+                       } catch (IOException e) {
+                           Toast.makeText(this, "While writing file: " + e, Toast.LENGTH_LONG).show();
+                       }
+                   }
+               }
+            }
+        }
+    }
+
+    private void writeSumsToIntergalStorage() throws IOException {
+        File sumsFile = getSumsFile();
+
+        try (PrintWriter pw = new PrintWriter(new FileWriter(sumsFile))) {
+            for (int i = 0; i < this.sums.getCount(); i++) {
+                Integer sum = this.sums.getItem(i);
+                pw.println(sum);
+            }
+        }
+    }
+
+    @NonNull
+    private File getSumsFile() {
+        File dataDir = this.getDataDir();
+        return new File(dataDir, "sums.txt");
+    }
+}
